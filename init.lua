abfedb51eb28cd92529767ce00a451e18599ec85
michalrzak <michal.r.zak@protonmail.com>
2025-07-26 09:02:41 +0200

    added python path

--- init.lua
+++ init.lua
@@ -204,6 +204,8 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+vim.g.python3_host_prog = '~/global-venvs/neovim/bin/python'
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
